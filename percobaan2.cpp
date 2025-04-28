6df5762456314b66e988c263efdd7ffd3d2a3523
ramdaniasyafitriani <ramdaniania04@gmail.com>
2025-04-28 19:10:50 +0700

    membuat oop luas persegi panjang

---
+++ percobaan2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+class persegipanjang
+{
+    public :
+        float panjang ;
+        float lebar ;
+        float luas ;
+
+        void inputData()
+        {
+            cout << "input panjang : ";
+            cin >> panjang;
+            cout << "input lebar : ";
+            cin >> lebar;
+        }
+        void menghitungluas()
+        {
+            luas = panjang * lebar;
+        }
+        void outputData()
+        {
+            cout << "luas persegi panjang = " << luas;
+        }
+
+};
+
+
